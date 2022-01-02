195c0596c5b831c614a489a185ca30c929968c7c
parkinhwa <inhwa6548@naver.com>
2022-01-02 20:02:48 +0900

    node.js basic

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const bodtParser = require("body-parser");
+const { User } = require("./models/User");
 const mongoose = require("mongoose");
+
+//application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose
   .connect(
     "mongodb+srv://inhwa:258963@login.carxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -10,5 +16,15 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+app.post("/register", (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
