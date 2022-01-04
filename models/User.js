187f3802140cf56c7de34d1e1c9b837be8fcbfbc
parkinhwa <inhwa6548@naver.com>
2022-01-04 21:16:35 +0900

    Auth 기능 추가

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -47,9 +48,43 @@ userSchema.pre("save", function (next) {
       });
     });
   } else {
-      next();
+    next();
   }
 });
 
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword === 암호화된 비밀번호
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 토큰 생성
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  // user._id + 'secertToken' = token
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  user._id + '' = token;
+  // 토큰 디코딩
+  jwt.verify(token, 'secretToken', function(err, decoded){
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    user.findOne({"_id": decoded, "token":token}, function(err, user){
+      if(err) return cb(err);
+      cb(null, user);
+    })
+  })
+}
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
