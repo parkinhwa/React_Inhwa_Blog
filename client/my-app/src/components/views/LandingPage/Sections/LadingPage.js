a171651b1edfb660724c535b18031c7fe09e7534
parkinhwa <65644486+parkinhwa@users.noreply.github.com>
2022-01-15 14:57:01 +0900

    Proxy 설정

--- client/my-app/src/components/views/LandingPage/Sections/LadingPage.js
+++ client/my-app/src/components/views/LandingPage/Sections/LadingPage.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function LadingPage() {
+   useEffect(() => {
+      axios.get("/api/hello").then((response) => {
+       console.log(response);
+      });
+    }, []);
     return (
         <div>
             LadingPage
