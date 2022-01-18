4727ff288e76caabc91ad786ef5e43402068a381
parkinhwa <inhwa6548@naver.com>
2022-01-18 18:19:54 +0900

    회원가입기능 추가

--- client/my-app/src/_reducer/user_reducer.js
+++ client/my-app/src/_reducer/user_reducer.js
@@ -1,5 +1,6 @@
 import {
 	LOGIN_USER,
+	REGISTER_USER
 } from '../_actions/types'
 
 export default function (state = {}, action){
@@ -8,6 +9,11 @@ export default function (state = {}, action){
 			return {
 				...state,
 				loginSuccess: action.payload,
+			}
+		case REGISTER_USER:
+			return {
+				...state,
+				register: action.payload
 			}
 		default:
 			return {
--- client/my-app/src/components/views/RegisterPage/RegisterPage.js
+++ client/my-app/src/components/views/RegisterPage/RegisterPage.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+  // navigate
+  const navigate = useNavigate();
+
+  // react-redux
+  const dispatch = useDispatch();
+
+  //useState
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  // handler
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+    // setPassword(event.target.value)
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(
+      `Email: ${Email}
+	   Password:${Password}`
+    );
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야합니다.");
+    }
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+
+    dispatch(registerUser(body)).then((res) => {
+      console.log(`register success: ${res.body}`);
+      if (res.payload.success) {
+        navigate("/login");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
