1e8e6b284a31094225f3eac741bd588b8dfed65b
parkinhwa <inhwa6548@naver.com>
2022-01-19 16:09:38 +0900

    redux action

--- client/my-app/src/_actions/user_action.js
+++ client/my-app/src/_actions/user_action.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {
-  LOGIN_USER, 
+  LOGIN_USER,
+  REGISTER_USER, 
 } from "./types";
 
 export function loginUser(dataToSubmit) {
@@ -12,4 +13,15 @@ export function loginUser(dataToSubmit) {
     type: LOGIN_USER,
     payload: request,
   };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post("/api/users/register", dataToSubmit)
+    .then((res) => res.data);
+
+	return {
+		type: REGISTER_USER,
+		payload: request,
+	}
 }
