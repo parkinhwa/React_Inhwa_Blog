2833a3329085c3edf78e7e82892e8484db48d7cf
parkinhwa <inhwa6548@naver.com>
2022-01-21 00:03:36 +0900

    Nav, Main 화면 추가

--- client/src/components/views/LandingPage/Sections/LadingPage.js
+++ client/src/components/views/LandingPage/Sections/LadingPage.js
@@ -1,48 +1,30 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import '../../../../css/LadingPage.css';
+import "../../../../css/LadingPage.css";
+import NavBar from "../../NavBar/NavBar";
 
 function LadingPage() {
   // navigate
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
       console.log(response);
     });
   }, []);
 
-  const onClickHandler = () => {
-    axios.get("/api/users/logout").then((response) => {
-      if (response.data.success) {
-        navigate("/login");
-      } else {
-        alert("로그아웃 하는데 실패 했습니다.");
-      }
-    });
-  };
-
   return (
     <div>
-    <div class="menu_bar">
-      <ul class="menu">
-         <li><a href="#">Home</a></li>
-         <li><a href="#">About</a></li>
-         <li><a href="#">Board</a></li>
-         <li><a href="#">Reference</a></li>
-         <li><a href="#">Contact</a></li>
-         <button>로그인</button>
-         <button>회원가입</button>
-         <button onClick={onClickHandler}>로그아웃</button>
-      </ul>
-     </div>
-     <nav>
-        <div className={"title"}>
-          <h1 className={"title_text"}>Inhwa's Blog</h1>
-        </div>
-     </nav>
-     </div>
+      <div>
+        <NavBar />
+      </div>
+      <div class="title">
+        <h1 class="title_text">Inhwa's Blog</h1>
+        <h1 class="title_text">Inhwa's Blog</h1>
+        <h4 class="title_content">안녕하세요 박인화 블로그롱가링가링입니다.</h4>
+      </div>
+    </div>
   );
 }
 
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../../../css/NavBar.css";
 
 function NavBar() {
-    return (
-        <div>
-            NavBar
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 하는데 실패 했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div class="menu_bar">
+      <ul class="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Board</a>
+        </li>
+        {/* <button>Login</button>
+           <button>Sign Up</button> */}
+        <button onClick={onClickHandler}>Logout</button>
+        <input type="checkbox" id="trigger"></input>
+        <label for="trigger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+        <div class="sidebar"></div>
+      </ul>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
