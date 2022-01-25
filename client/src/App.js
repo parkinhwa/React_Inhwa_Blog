897de62ebf68cbde4939ec7e4a0fec0367850db9
parkinhwa <inhwa6548@naver.com>
2022-01-25 21:13:59 +0900

    NavBarPage 추가

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,19 @@ import {
 import LandingPage from "./components/views/LandingPage/Sections/LadingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import NavBarPage from "./components/views/NavBar/NavBar";
 import Auth from "./hoc/auth";
 
 function App() {
   const Auth_LadingPage = Auth(LandingPage, null);
   const Auth_LoginPage = Auth(LoginPage, false);
   const Auth_RegisterPage = Auth(RegisterPage, false);
+  const Auth_NavBarPage = Auth(NavBarPage, false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Auth_LadingPage/>} />
+        <Route exact path="/" element={<Auth_NavBarPage/>} />
+        <Route exact path="/home" element={<Auth_LadingPage/>}/>
         <Route exact path="/login" element={<Auth_LoginPage/>} />
         <Route exact path="/register" element={<Auth_RegisterPage/>} />
       </Routes>
